7703ac899c9add457d31f18e72da7a648b06ea14
unknown <mathiasimagine@gmail.com>
2022-02-27 13:47:10 -0300

    feat add exercise 3

--- clases/homeworks/ejercicio 3.ts
+++ clases/homeworks/ejercicio 3.ts
@@ -10,7 +10,7 @@ let person: [string, number] = ["Franco", 26];
 //let extendIncorrectPerson: [string, number] = ["Franco", 26, true]; // Error
 
 //crea una tupla, donde en la primera posicion haya un booleano y en la segunda un string
-
+let newTupla : [boolean,string] =[true, "mathi"]
 //crea un arreglo de strings
-
+let newArray: string[]= ["mati","ledesma"]
 export {}
